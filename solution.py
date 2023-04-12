cab3f50022d4923a8833224385d50a9ae0a16b69
ryzhval <130060842+ryzhval@users.noreply.github.com>
2023-04-13 01:42:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -32,7 +32,7 @@ def solution(control: np.array, test: np.array) -> bool:
 #       return False
 
     #res = (w.ztest(control, test, alternative = 'two-sided').pvalue < alpha)
-    _, pvalue = stats.ttest_ind(control, test, equal_var = False, alternative = 'two-sided')
+    _, pvalue = stats.ttest_ind(control, test, equal_var = False, alternative = 'greater')
     #return res
     return pvalue <= 0.01
     
