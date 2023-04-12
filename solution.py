aec6c460a07e3d99f3a88d63bababa1751f315dc
ryzhval <130060842+ryzhval@users.noreply.github.com>
2023-04-13 00:21:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -31,7 +31,7 @@ def solution(control: np.array, test: np.array) -> bool:
 #     else: 
 #       return False
 
-    res = (stats.ttest_ind(control, test, alternative = 'less').pvalue < alpha)
+    res = (stats.ttest_rel(control, test, alternative = 'two-sided').pvalue < alpha)
     return res
     
     
