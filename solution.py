ca4639d38fb15c3d00df6ee7da0adf8a8235fe65
ryzhval <130060842+ryzhval@users.noreply.github.com>
2023-04-12 13:48:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,31 +5,33 @@ import math
 
 chat_id = 371649437 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
+def solution(control: np.array, test: np.array) -> bool:
     
      # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     # Ваш ответ, True или False
-    alpha=0.01
+    alpha = 0.01
     
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    p_comb = (x_success + y_success) / (x_cnt + y_cnt)
-    difference = p1 - p2
+#     p1 = x_success / x_cnt
+#     p2 = y_success / y_cnt
+#     p_comb = (x_success + y_success) / (x_cnt + y_cnt)
+#     difference = p1 - p2
  
-    z_value = difference / math.sqrt(
-        p_comb * (1 - p_comb) * (1 / x_success + 1 / y_success)
-    ) 
-    distr = stats.norm(0, 1)  
+#     z_value = difference / math.sqrt(
+#         p_comb * (1 - p_comb) * (1 / x_success + 1 / y_success)
+#     ) 
+#     distr = stats.norm(0, 1)  
     
-    p_value = (1 - distr.cdf(abs(z_value))) * 2 
+#     p_value = (1 - distr.cdf(abs(z_value))) * 2 
     
-    if (p_value < alpha):
-      return True
-    else: 
-      return False
+#     if (p_value < alpha):
+#       return True
+#     else: 
+#       return False
+
+    res = (stats.ttest_ind(control, test).pvalue < alpha)
+    return res
+    
+    
